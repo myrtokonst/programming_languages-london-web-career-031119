324e605ed82d4465c827a6c096c3f16ec5c4c9f8
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-09 19:34:08 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,49 @@
+require 'pry'
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+ 
+  }
+}
+
 def reformat_languages(languages)
-  # your code here
+  hash = {}
+languages.each do |style, names|
+  if style == :oo
+    names.each do |name, info|
+    hash[name]= info
+    hash[name][:style] = [:oo]
+  end 
+else  names.each do |name, info|
+    hash[name]= info
+    hash[name][:style] = [:functional]
+end
+end
+end
+hash
 end
